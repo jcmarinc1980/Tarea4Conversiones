6d76983eadc35975978962531a331ccabd92e4da
jcmarinc1980 <juan.marin.cascante@est.una.ac.cr>
2024-05-14 19:21:42 -0600

    aaa

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,6 +5,13 @@
     <button class="button" @click="irATemperatura">Conversiones de Temperatura</button>
     <button class="button" @click="irAPeso">Conversiones de Peso</button>
   </div>
+
+  <!-- Pie de página -->
+  <footer>
+    <div class="left">Programación Web</div>
+    <div class="center">Tarea 4 Conversiones Vue.js</div>
+    <div class="right">Juan Carlos Marín Cascante <br class="mobile-break">@2024</div>
+  </footer>
 </template>
 
 <script>
@@ -35,5 +42,49 @@ export default {
   max-width: 960px; /* Limita el ancho del contenedor */
   padding: 2rem; /* Agrega un relleno interno */
   text-align: center; /* Centra el texto dentro del contenedor */
+}
+
+/* Estilos para el pie de página */
+footer {
+  position: fixed; /* Fijar al final de la pantalla */
+  bottom: 0; /* Fijar al borde inferior de la pantalla */
+  width: 100%; /* Ancho completo */
+  background-color: #413f3f; /* Color de fondo gris claro */
+  padding: 1rem; /* Espaciado interno */
+  box-shadow: 0 -5px 5px rgba(0, 0, 0, 0.1); /* Sombra */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left, .center, .right {
+  color: white; /* Letras blancas */
+}
+
+.left {
+  text-align: left;
+}
+
+.center {
+  text-align: center;
+}
+
+.right {
+  text-align: right;
+}
+
+/* Estilos para el salto de línea en dispositivos móviles */
+.mobile-break {
+  display: none; /* Oculta el salto de línea en pantallas grandes */
+}
+
+@media (max-width: 600px) {
+  .right {
+    display: block; /* Muestra el salto de línea en dispositivos móviles */
+  }
+
+  .mobile-break {
+    display: block; /* Muestra el salto de línea en dispositivos móviles */
+  }
 }
 </style>
